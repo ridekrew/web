cc65bcfc063d9f2ceedf0831bfc6ba9b58907b1f
Shovit Kandel <shovit.kandel@gmail.com>
2017-05-11 11:23:07 -0500

    fixed index.html to load all dependencies

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 var express = require('express');
-var mongoose = require('mongoose');
 var path = require('path');
 var root = __dirname;
 
-require('dotenv').config()
-
 var app = express();
-
-mongoose.connect(process.env.MONGO_URI);
 
 //set port
 var port = process.env.PORT || 8000
@@ -15,9 +10,9 @@ var port = process.env.PORT || 8000
 
 app.use(express.static(path.join(root, 'client')))
 app.use(express.static(path.join(root, 'node_modules')))
-//app.use(express.static(path.join(root, 'bower_components')))
+// app.use(express.static(path.join(root, 'bower_components')))
 
 
 app.listen(port, function(){
-	console.log('listening in port ' + port)
+	console.log('listening in port 8000')
 })
