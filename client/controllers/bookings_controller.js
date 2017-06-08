cd5a584a60c4bedda916a42f39a45dc4c48d5c12
Austin Osborne <ffosb22@gmail.com>
2017-06-07 21:22:24 -0500

    Added Google Maps pricing model

--- client/controllers/bookings_controller.js
+++ client/controllers/bookings_controller.js
@@ -80,7 +80,34 @@ app.controller('booking_controller', function($scope, $http, $location, BookingF
     $scope.addSlide();
   }
 
-  	$scope.price = 100;
+  	function calculatePrice() {
+		var origin = ['3108 Centerville Rd., Wilmington, DE'];
+		var destination = ['2949 Parkwood Blvd., Frisco, TX'];
+
+		var distanceMatrix = new google.maps.DistanceMatrixService();
+		var distanceRequest = { origins: origin, destinations: destination, travelMode: google.maps.TravelMode.DRIVING, unitSystem: google.maps.UnitSystem.IMPERIAL, avoidHighways: false, avoidTolls: false };
+		var price = 0;
+		distanceMatrix.getDistanceMatrix(distanceRequest, function(response, status) {
+			if (status != google.maps.DistanceMatrixStatus.OK) {
+				console.log("There was an error.");
+			} else {
+				var responseFields = response.rows[0].elements[0];
+				var distance = responseFields.distance.value / 1609.344; // Convert meters to miles for use in pricing model
+				var duration = responseFields.duration.value / 60.0; // Convert seconds to minutes
+				if (distance <= 100) {
+					price = ((0.5 * duration + 0.2 * distance) * 1.4) + 1.5
+				} else if (distance <= 200) {
+					price = ((0.44 * duration + 0.18 * distance) * 1.325) + 1.5
+				} else {
+					price = ((0.4166 * duration + 0.16 * distance) * 1.25) + 1.5
+				}
+			}
+			console.log(price);
+			return price;
+		});
+	}
+
+	$scope.price = calculatePrice();
 
 
 	
