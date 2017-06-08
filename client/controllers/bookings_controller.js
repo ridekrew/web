e58e27b7fb6bbb2460e443f6202df1a201dbfd9d
Shovit Kandel <shovit.kandel@gmail.com>
2017-06-07 22:01:49 -0500

    added booking toggle

--- client/controllers/bookings_controller.js
+++ client/controllers/bookings_controller.js
@@ -22,7 +22,10 @@ app.controller('booking_controller', function($scope, $http, $location, BookingF
 	$scope.gPlace;
 	//Creating toggle for form
 	
-	$scope.show = 0;
+	$scope.show = false;
+	$scope.toggle = function(){
+		$scope.show = !$scope.show;
+	}
 
 	$scope.date = new Date();
 
@@ -45,40 +48,6 @@ app.controller('booking_controller', function($scope, $http, $location, BookingF
 		$location.url('/submit')
 	}
 	
-	//ADD CAROUSEL
-	var baseURL = '/static/img/';
-	// $scope.myInterval = 5000;
-	$scope.noWrapSlides = false;
-	$scope.active = 0;
-	$scope.slides = [];
-	var currIndex = 0;
-
-	$scope.addSlide = function() {
-		if ($scope.slides.length == 0){
-			x = baseURL + 'car_background.png'
-		};
-		if ($scope.slides.length == 1){
-			x = baseURL + 'gradient-wallpaper-2.jpg'
-		};
-		if ($scope.slides.length == 2){
-			x = baseURL + 'gradient-wallpaper-3.jpg'
-		}; 
-	// var newWidth = 600 + slides.length + 1;
-	$scope.slides.push({
-	  image: x,
-	  // image: '//unsplash.it/' + newWidth + '/300',
-	  text: [("<div class = 'absolute-div'><h2>Travel Intercity.</h2><p>Book a long distance trip</p><p>for a hyper-affordable price</p><a href = '#!/promotions'><button>View Promotions</button></a></div><img id = 'car' src = 'static/img/test.png'>"), 
-	  		("<div class = 'absolute-div'><h2>Never wait after a flight again.</h2><p>Call a Krew Driver before flight</p><p>and travel with ease</p><button>Book Now</button></div><img id = 'car' src = 'static/img/jet.png'>"),
-	  		("<div class = 'absolute-div'><h2>Travel Intercity.</h2><p>Book a long distance trip</p><p>for a hyper-affordable price</p><button>View Promotions</button></div><img id = 'car' src = 'static/img/test.png'>")
-	  ][$scope.slides.length % 3],
-	  id: currIndex++,
-	  trustedText: $sce.trustAsHtml($scope.slides.text)
-	    });
-  };
-
-  for (var i = 0; i < 3; i++) {
-    $scope.addSlide();
-  }
 
     $scope.price = calculatePrice();
   
@@ -113,37 +82,7 @@ app.controller('booking_controller', function($scope, $http, $location, BookingF
 		return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
 	}
 
-	
-	// Sripre Response Handler
-// 	$scope.stripeCallback = function (code, result) {
-// 		console.log('callback function')
-//     if (result.error) {
-//         window.alert('it failed! error: ' + result.error.message);
-//     } else {
-//         window.alert('success! token: ' + result.id);
-//     }
-// };
-
 });
 
 app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
 
-// app.config(function($window) {
-// 	$window.Stripe.setPublishableKey('sk_live_xzZpoV0uUICrcg79YFg3NtGj');
-
-// 	$scope.stripeCallback = function (code, result) {
-//     if (result.error) {
-//         window.alert('it failed! error: ' + result.error.message);
-//     } else {
-//         window.alert('success! token: ' + result.id);
-//     }
-// };
-// })
-// stripe
-// private Key : sk_live_xzZpoV0uUICrcg79YFg3NtGj
-// publishable : pk_live_t34DtKRcABYPKqN4gLZgYoxx
-
-// test
-// publish key : pk_test_Ejljklt0l8hJEa3t0y3m8jlU
-// secret key  : sk_test_3yi7zXSDgar9qi139UrRRYtt
-// 
